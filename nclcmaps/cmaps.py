c79401810e881c850e9146393c595bad2f8b07d2
hhuangmeso <hhuangmeso@gmail.com>
2016-04-03 13:01:25 +0800

    bug fix

--- nclcmaps/cmaps.py
+++ nclcmaps/cmaps.py
@@ -7,7 +7,7 @@ def _coltbl(name):
     cmaps_dir = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), 'colormaps')
     cmap_file = os.path.join(cmaps_dir, name+'.rgb')
-    rgb = np.loadtxt(cmap_file, skiprows=6)/255
+    rgb = np.loadtxt(cmap_file, skiprows=2)/255
     return rgb
 
 
