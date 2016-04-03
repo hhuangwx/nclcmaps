8e209630ac057d2a53afc9f708bbf76b22de5b93
wqshen <wqshen91@gmail.com>
2016-04-03 13:48:49 +0800

    revise edition

--- nclcmaps/cmaps.py
+++ nclcmaps/cmaps.py
@@ -1,14 +1,20 @@
 import numpy as np
 import os
+import re
 from matplotlib.colors import ListedColormap
 
 
 def _coltbl(name):
     cmaps_dir = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'colormaps')
+                os.path.dirname(os.path.abspath(__file__)), 'colormaps')
     cmap_file = os.path.join(cmaps_dir, name+'.rgb')
-    rgb = np.loadtxt(cmap_file, skiprows=2)/255
-    return rgb
+    pattern = re.compile(r'\s*(\d[\d\.]*)\s+(\d[\d\.]*)\s+(\d[\d\.]*).*')
+    with open(cmap_file) as cmap:
+        cmap_buff = cmap.read()
+    if re.search(r'\s*\d\.\d*', cmap_buff):
+        return np.asarray(pattern.findall(cmap_buff), 'f4')
+    else:
+        return np.asarray(pattern.findall(cmap_buff), 'u1')/255.
 
 
 def cmaps(name):
