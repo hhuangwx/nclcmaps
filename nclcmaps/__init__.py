7016c8e03c827106ddaa66bfffc3470824edbfa9
hhuangmeso <hhuangmeso@gmail.com>
2016-04-05 12:55:26 +0800

    list cmap names

--- nclcmaps/__init__.py
+++ nclcmaps/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: hhuang
 # @Date:   2016-04-03 11:29:03
-# @Last Modified by:   hhuang
-# @Last Modified time: 2016-04-03 11:37:52
+# @Last Modified by:   hhuangmeso
+# @Last Modified time: 2016-04-05 12:54:16
 
 from .cmaps import cmaps
+from .cmaps import listname
--- nclcmaps/cmaps.py
+++ nclcmaps/cmaps.py
@@ -1,13 +1,16 @@
 import numpy as np
 import os
+import glob
 import re
 from matplotlib.colors import ListedColormap
 
+CMAPSDIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'colormaps')
+
 
 def _coltbl(name):
-    cmaps_dir = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), 'colormaps')
-    cmap_file = os.path.join(cmaps_dir, name+'.rgb')
+
+    cmap_file = os.path.join(CMAPSDIR, name+'.rgb')
     pattern = re.compile(r'(\d\.?\d*)\s+(\d\.?\d*)\s+(\d\.?\d*)\n')
     with open(cmap_file) as cmap:
         cmap_buff = cmap.read()
@@ -15,6 +18,12 @@ def _coltbl(name):
         return np.asarray(pattern.findall(cmap_buff), 'f4')
     else:
         return np.asarray(pattern.findall(cmap_buff), 'u1')/255.
+
+
+def listname():
+    cmapsflist = sorted(glob.glob(CMAPSDIR+'/*.rgb'))
+    for fname in cmapsflist:
+        print(os.path.basename(fname)[:-4])
 
 
 def cmaps(name):
